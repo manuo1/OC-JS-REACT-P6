6dced5f05be7037ef7686b9e304a7e8ecd1dbbaa
manuo1 <manuo1@free.fr>
2025-04-04 20:52:43 +0200

    Add mediaTemplate

---
+++ scripts/templates/media.js
@@ -0,0 +1,40 @@
+function mediaTemplate(data) {
+  const { title, image, video, likes, photographerId, id } = data;
+  const mediaPath = `assets/photographers/media/${photographerId}/${image || video}`;
+  const imageType = "image";
+  const videoType = "video";
+  const mediaType = image ? imageType : videoType;
+
+  function getMediaCard() {
+    const article = document.createElement("article");
+    article.classList.add("media-card");
+
+    article.innerHTML = `
+        <a href="${mediaPath}" class="media-card__link" aria-label="${title}" data-id="${id}">
+          ${
+            mediaType === imageType
+              ? `<img src="${mediaPath}" alt="${title}" class="media-card__media" />`
+              : `<video class="media-card__media" title="${title}" aria-label="${title}">
+              <source src="${mediaPath}" type="video/mp4" />
+              Votre navigateur ne supporte pas les vidéos HTML5.
+            </video>`
+          }
+        </a>
+        <div class="media-card__info">
+          <h2 class="media-card__title">${title}</h2>
+          <div class="media-card__likes">
+            <span class="media-card__likes-count" aria-label="${likes} likes">${likes}</span>
+            <button class="media-card__like-button" aria-label="Aimer ${title}" data-liked="false">
+              &#10084;
+            </button>
+          </div>
+        </div>
+      `;
+
+    return article;
+  }
+
+  return { getMediaCard };
+}
+
+export { mediaTemplate };
